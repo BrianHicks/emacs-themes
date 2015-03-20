ead911bac1736c409906663df018ac8e03b4db6b
Brian Hicks <brian@brianthicks.com>
2015-03-20 12:06:56 -0500

    remove green (strings) and red (mismatch) from regular rainbow delimiters

--- base16-ocean-brian-theme.el
+++ base16-ocean-brian-theme.el
@@ -92,11 +92,11 @@
    ;; rainbow-delimiters
    `(rainbow-delimiters-depth-1-face ((t (:foreground ,purple))))
    `(rainbow-delimiters-depth-2-face ((t (:foreground ,slate))))
-   `(rainbow-delimiters-depth-3-face ((t (:foreground ,teal))))
-   `(rainbow-delimiters-depth-4-face ((t (:foreground ,green))))
+   `(rainbow-delimiters-depth-3-face ((t (:foreground ,teal-light))))
+   `(rainbow-delimiters-depth-4-face ((t (:foreground ,teal-dark))))
    `(rainbow-delimiters-depth-5-face ((t (:foreground ,yellow))))
-   `(rainbow-delimiters-depth-6-face ((t (:foreground ,orange))))
-   `(rainbow-delimiters-depth-7-face ((t (:foreground ,red))))
+   `(rainbow-delimiters-depth-6-face ((t (:foreground ,orange-light))))
+   `(rainbow-delimiters-depth-7-face ((t (:foreground ,orange-dark))))
    `(rainbow-delimiters-depth-8-face ((t (:foreground ,brown))))
    `(rainbow-delimiters-depth-9-face ((t (:foreground ,base07))))
 
